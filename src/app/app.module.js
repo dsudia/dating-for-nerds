2659dd1bdaf3d483cfacd6a292dbeea88e13a1a9
David Sudia <dsudia@gmail.com>
2016-04-19 14:03:56 -0600

    home page renders using ui-router from directive

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -2,7 +2,17 @@
 
   'use strict';
 
-  angular.module('gDating', ['ui.router']);
+  angular.module('gDating', ['ui.router', 'angular-refills']);
+
+  angular.module('gDating').config(($stateProvider, $urlRouterProvider) => {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+      .state('landing', {
+        url: '/',
+        template: '<landing></landing>'
+      });
+  });
 
 })();
 

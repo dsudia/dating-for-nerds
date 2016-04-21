91db76a7ccbd1e27441e4655973a134c2ed8971d
David Sudia <dsudia@gmail.com>
2016-04-21 14:19:22 -0600

    age and any/all still need work

--- src/app/components/search/search.directive.js
+++ src/app/components/search/search.directive.js
@@ -6,13 +6,10 @@
     return {
       restrict: 'E',
       templateUrl: 'app/components/search/search.view.html',
-      controller: function($scope) {
+      controller: function($rootScope, $scope) {
         $scope.ages = [18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40];
-        $scope.searchAgeLow = 0;
-        $scope.searchAgeHigh = 0;
-        $scope.searchType = true;
+        $rootScope.search = {};
       }
     }
   });
-
 })();
---
+++ src/app/components/search/search.filter.js
@@ -0,0 +1,12 @@
+(function() {
+
+  'use strict';
+
+  angular.module('gDating').filter('searchFilter', searchFilter);
+
+  searchFilter.$inject = [];
+
+  function searchFilter() {
+  }
+
+})();
